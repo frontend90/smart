b1e903fbc51be891b065f585ae517a218bc3925c
frontend90 <leon44173@gmail.com>
2020-01-20 19:21:26 +0200

    сверстан clients

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*******    slick    ********************/
+$('.clients-slider').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  prevArrow: $('.arrow-left'),
+  nextArrow: $('.arrow-right'),
+  dots: true 
+});
