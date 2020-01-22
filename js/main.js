88b92216818a7ef110c993b7c744556d0806b042
frontend90 <leon44173@gmail.com>
2020-01-22 17:46:48 +0200

    внесены изменения

--- js/main.js
+++ js/main.js
@@ -4,5 +4,39 @@ $('.clients-slider').slick({
   slidesToScroll: 2,
   prevArrow: $('.arrow-left'),
   nextArrow: $('.arrow-right'),
-  dots: true 
+  dots: true
+});
+
+/**************     dropdown     **************/
+$('.dropdown').click(function () {
+  $('.menu').slideToggle(1000);
+  $('.menu').addClass('active');
+});
+
+$('.menu a').click(function () {
+  $('.menu').hide(1000);
+  $('.menu').removeClass('active');
+});
+
+$('.close').click(function () {
+  $('.menu').hide(1000);
+  $('.menu').removeClass('active');
+});
+
+/*********************     плавный скролл        ************************/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
 });
